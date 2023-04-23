f70c90b18df5d149c0f29306eff7adbf5424684f
amayesingnathan <amayesingnathan123@gmail.com>
2023-04-23 17:26:15 +0100

    Added API project for accessing database

---
+++ GungeonApp.API/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using GungeonApp.Database;
+using GungeonApp.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GungeonApp.API.Controllers
+{
+    public class SearchController : Controller
+    {
+        [Route("search/{name}")]
+        [HttpGet]
+        public IActionResult Search(string name)
+        {
+            try
+            {
+                var items = GungeonDB.MatchItem(name);
+                if (items is null || items.Length == 0)
+                {
+                    return BadRequest($"Could not locate any items matching name: {name}");
+                }
+
+                return Ok(items);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}
